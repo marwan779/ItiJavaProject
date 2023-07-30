cb46cad7e0ef50aee1fb27615d420b045c6a047b
marwan779 <140047712+marwan779@users.noreply.github.com>
2023-07-30 18:38:51 +0300

    declaration  variables
    setting the initial values for some variables

--- DrawingApplet.java
+++ DrawingApplet.java
@@ -4,6 +4,15 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class DrawingApplet extends Applet implements ActionListener, MouseListener, MouseMotionListener  {
-   
+ private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 400;
+
+    private Panel topPanel;
+    private Button rectButton, circleButton, ovalButton, lineButton, yellowButton, blueButton, greenButton, fillButton, eraseButton, increaseButton, decreaseButton,clearButton;
+    private Color currentColor = Color.BLACK;
+    private String currentShape = "";
+    private int startX, startY, endX, endY, lineWidth = 1;
+    private boolean isDrawing = false;
+    private boolean isErasing = false;   
 
 }
